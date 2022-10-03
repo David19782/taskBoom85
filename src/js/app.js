4eba87ea34ebf56548d4fc63fc01183d3d601e4c
David19782 <yordanovdavid@gmail.com>
2022-10-03 17:00:52 +0300

    feat: add limit

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const url = "https://pokeapi.co/api/v2/pokemon";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=10";
   const ul = document.querySelector("ul");
   fetch(url)
   .then(res => res.json())
