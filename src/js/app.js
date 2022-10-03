5c8056f5bdf8fa4a1f9751df37888dcb4c7846e9
David19782 <yordanovdavid@gmail.com>
2022-10-03 16:53:33 +0300

    feat: add Pokemon

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,18 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const url = "https://pokeapi.co/api/v2/pokemon";
   const ul = document.querySelector("ul");
+  fetch(url)
+  .then(res => res.json())
+  .then(data => {
+    //console.log(data);
+    for(let i = 0; i < 10; i++){
+      const li = document.createElement('li');
+      li.innerText = data.results[i].name;
+      ul.appendChild(li);
+    }
+    
+  })
+
 });
